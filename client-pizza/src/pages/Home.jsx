1e61337635263c06ffb3867251a84002b1bd229e
RampantStars <klyginwebdev@mail.ru>
2022-09-07 10:47:10 +0300

    Pizza House

---
+++ client-pizza/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://62e276b3e8ad6b66d85c02f7.mockapi.io/pizzas')
+      .then((response) => response.json())
+      .then((json) => {
+        setPizzas(json);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : pizzas.map((item) => <PizzaBlock key={item.id} {...item} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
