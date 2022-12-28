67f190620434b27e9f8801bfa48d1c43a722da74
RampantStars <klyginwebdev@mail.ru>
2022-12-28 17:32:25 +0300

    The database is connected

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common/decorators/modules';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'pizzaDb',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
