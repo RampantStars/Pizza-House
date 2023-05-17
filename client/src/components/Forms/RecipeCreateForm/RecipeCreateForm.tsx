bfd6e19da39f8de04280b3bb2381892ac641e2c6
RampantStars <klyginwebdev@mail.ru>
2023-05-17 15:23:18 +0300

    Поправлено модальное окно Рецепта

--- client/src/components/Forms/RecipeCreateForm/RecipeCreateForm.tsx
+++ client/src/components/Forms/RecipeCreateForm/RecipeCreateForm.tsx
@@ -99,92 +99,98 @@ export const RecipeCreateForm = () => {
     <div className={styles.recipeCreateForm}>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.form__container}>
-          <label className={styles.label}>
-            Картинка рецепта
-            {!image ? (
-              <svg
-                className={styles.input__svg}
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <title />
+          <div className={styles.form__top}>
+            <label className={styles.label}>
+              Картинка рецепта
+              {!image ? (
+                <svg
+                  className={styles.input__svg}
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <title />
 
-                <g id="Complete">
-                  <g id="upload">
-                    <g>
-                      <path
-                        d="M3,12.3v7a2,2,0,0,0,2,2H19a2,2,0,0,0,2-2v-7"
-                        fill="none"
-                        stroke="#000000"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                      />
-
+                  <g id="Complete">
+                    <g id="upload">
                       <g>
-                        <polyline
-                          data-name="Right"
+                        <path
+                          d="M3,12.3v7a2,2,0,0,0,2,2H19a2,2,0,0,0,2-2v-7"
                           fill="none"
-                          id="Right-2"
-                          points="7.9 6.7 12 2.7 16.1 6.7"
                           stroke="#000000"
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth="2"
                         />
 
-                        <line
-                          fill="none"
-                          stroke="#000000"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          x1="12"
-                          x2="12"
-                          y1="16.3"
-                          y2="4.8"
-                        />
+                        <g>
+                          <polyline
+                            data-name="Right"
+                            fill="none"
+                            id="Right-2"
+                            points="7.9 6.7 12 2.7 16.1 6.7"
+                            stroke="#000000"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                          />
+
+                          <line
+                            fill="none"
+                            stroke="#000000"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            x1="12"
+                            x2="12"
+                            y1="16.3"
+                            y2="4.8"
+                          />
+                        </g>
                       </g>
                     </g>
                   </g>
-                </g>
-              </svg>
-            ) : (
-              <img
-                alt="Загруженная картинка"
-                className={styles.img}
-                src={image}
-                onLoad={handleImageLoad}
+                </svg>
+              ) : (
+                <img
+                  alt="Загруженная картинка"
+                  className={styles.img}
+                  src={image}
+                  onLoad={handleImageLoad}
+                />
+              )}
+              <input
+                type="file"
+                {...register('image')}
+                onChange={onImageChange}
+                className={styles.input__img}
               />
-            )}
-            <input
-              type="file"
-              {...register('image')}
-              onChange={onImageChange}
-              className={styles.input__img}
-            />
-            <p className={styles.error}>{errors.image?.message}</p>
-          </label>
-          <div className={styles.form__left}>
-            <label className={styles.label}>
-              Название рецепта
-              <input type="text" {...register('name')} />
-              <p className={styles.error}>{errors.name?.message}</p>
+              <p className={styles.error}>{errors.image?.message}</p>
             </label>
-            <label className={styles.label}>
-              Цена рецепта
-              <input type="number" {...register('price')} />
-              <p className={styles.error}>{errors.price?.message}</p>
-            </label>
-            <label className={styles.label}>
-              Описание рецепта
-              <input type="text" {...register('description')} />
-            </label>
-            <label className={styles.label}>
-              Скидка рецепта
-              <input type="number" {...register('salePercent')} />
-            </label>
+            <div className={styles.top__group}>
+              <label className={styles.label}>
+                Название рецепта
+                <input type="text" {...register('name')} />
+                <p className={styles.error}>{errors.name?.message}</p>
+              </label>
+              <label className={styles.label}>
+                Цена рецепта
+                <input type="number" {...register('price')} />
+                <p className={styles.error}>{errors.price?.message}</p>
+              </label>
+            </div>
+            <div className={styles.top__group}>
+              <label className={styles.label}>
+                Описание рецепта
+                <input type="text" {...register('description')} />
+                <p className={styles.error}>{errors.description?.message}</p>
+              </label>
+              <label className={styles.label}>
+                Скидка рецепта
+                <input type="number" {...register('salePercent')} />
+                <p className={styles.error}>{errors.salePercent?.message}</p>
+              </label>
+            </div>
           </div>
-          <div className={styles.form__right}>
+          <div className={styles.form__bottom}>
             <label className={styles.label}>
               Доступные категории рецепта
               <Controller
--- client/src/components/Forms/RecipeEditForm/RecipeEditForm.tsx
+++ client/src/components/Forms/RecipeEditForm/RecipeEditForm.tsx
@@ -114,67 +114,115 @@ export const RecipeEditForm = () => {
     <div className={styles.recipeCreateForm}>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.form__container}>
-          <label className={styles.label}>
-            Картинка рецепта
-            {!image ? (
-              <img
-                alt="Загруженная картинка"
-                className={styles.img}
-                src={`http://localhost:5000/${editingRecipe.imageUrl}`}
-                onLoad={handleImageLoad}
+          <div className={styles.form__top}>
+            <label className={styles.label}>
+              Картинка рецепта
+              {!image ? (
+                <svg
+                  className={styles.input__svg}
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <title />
+
+                  <g id="Complete">
+                    <g id="upload">
+                      <g>
+                        <path
+                          d="M3,12.3v7a2,2,0,0,0,2,2H19a2,2,0,0,0,2-2v-7"
+                          fill="none"
+                          stroke="#000000"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                        />
+
+                        <g>
+                          <polyline
+                            data-name="Right"
+                            fill="none"
+                            id="Right-2"
+                            points="7.9 6.7 12 2.7 16.1 6.7"
+                            stroke="#000000"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                          />
+
+                          <line
+                            fill="none"
+                            stroke="#000000"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            x1="12"
+                            x2="12"
+                            y1="16.3"
+                            y2="4.8"
+                          />
+                        </g>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              ) : (
+                <img
+                  alt="Загруженная картинка"
+                  className={styles.img}
+                  src={image}
+                  onLoad={handleImageLoad}
+                />
+              )}
+              <input
+                type="file"
+                {...register('image')}
+                onChange={onImageChange}
+                className={styles.input__img}
               />
-            ) : (
-              <img
-                alt="Загруженная картинка"
-                className={styles.img}
-                src={image}
-                onLoad={handleImageLoad}
-              />
-            )}
-            <input
-              type="file"
-              {...register('image')}
-              onChange={onImageChange}
-              className={styles.input__img}
-            />
-            <p className={styles.error}>{errors.image?.message}</p>
-          </label>
-          <div className={styles.form__left}>
-            <label className={styles.label}>
-              Название рецепта
-              <input type="text" {...register('name')} />
-              <p className={styles.error}>{errors.name?.message}</p>
+              <p className={styles.error}>{errors.image?.message}</p>
             </label>
-            <label className={styles.label}>
-              Цена рецепта
-              <input type="number" {...register('price')} />
-              <p className={styles.error}>{errors.price?.message}</p>
-            </label>
-            <label className={styles.label}>
-              Описание рецепта
-              <input type="text" {...register('description')} />
-            </label>
-            <label className={styles.label}>
-              Скидка рецепта
-              <input type="number" {...register('salePercent')} />
-            </label>
+            <div className={styles.top__group}>
+              <label className={styles.label}>
+                Название рецепта
+                <input type="text" {...register('name')} />
+                <p className={styles.error}>{errors.name?.message}</p>
+              </label>
+              <label className={styles.label}>
+                Цена рецепта
+                <input type="number" {...register('price')} />
+                <p className={styles.error}>{errors.price?.message}</p>
+              </label>
+            </div>
+            <div className={styles.top__group}>
+              <label className={styles.label}>
+                Описание рецепта
+                <input type="text" {...register('description')} />
+                <p className={styles.error}>{errors.description?.message}</p>
+              </label>
+              <label className={styles.label}>
+                Скидка рецепта
+                <input type="number" {...register('salePercent')} />
+                <p className={styles.error}>{errors.salePercent?.message}</p>
+              </label>
+            </div>
           </div>
-          <div className={styles.form__right}>
+          <div className={styles.form__bottom}>
             <label className={styles.label}>
               Доступные категории рецепта
               <Controller
                 control={control}
                 name="categories"
+                rules={{ required: 'Это поле обязательное' }}
                 render={({ field: { onChange }, fieldState: { error } }) => (
                   <div>
                     <div className={styles.select__group}>
                       <Select
                         className={styles.select}
                         options={categories}
+                        defaultValue={editingRecipe.categories}
                         getOptionLabel={(category: Category) => category.name}
                         getOptionValue={(category: Category) => category.name}
-                        defaultValue={editingRecipe.categories}
                         filterOption={(categories) => categories.label !== 'Все'}
+                        captureMenuScroll
                         closeMenuOnSelect={false}
                         onChange={(newValue) => onChange(newValue.map((value) => value.name))}
                         components={animatedComponents}
@@ -205,15 +253,16 @@ export const RecipeEditForm = () => {
               <Controller
                 control={control}
                 name="sizes"
-                render={({ field: { value, onChange }, fieldState: { error } }) => (
+                rules={{ required: 'Это поле обязательное' }}
+                render={({ field: { onChange }, fieldState: { error } }) => (
                   <div>
                     <div className={styles.select__group}>
                       <Select
                         className={styles.select}
                         options={sizes}
+                        defaultValue={editingRecipe.sizes}
                         getOptionLabel={(size: Size) => size.name}
                         getOptionValue={(size: Size) => size.name}
-                        defaultValue={editingRecipe.sizes}
                         closeMenuOnSelect={false}
                         onChange={(newValue) => onChange(newValue.map((value) => value.name))}
                         components={animatedComponents}
@@ -244,15 +293,16 @@ export const RecipeEditForm = () => {
               <Controller
                 control={control}
                 name="doughTypes"
+                rules={{ required: 'Это поле обязательное' }}
                 render={({ field: { onChange }, fieldState: { error } }) => (
                   <div>
                     <div className={styles.select__group}>
                       <Select
                         className={styles.select}
                         options={doughTypes}
+                        defaultValue={editingRecipe.doughtTypes}
                         getOptionLabel={(doughType: DoughType) => doughType.name}
                         getOptionValue={(doughType: DoughType) => doughType.name}
-                        defaultValue={editingRecipe.doughtTypes}
                         closeMenuOnSelect={false}
                         onChange={(newValue) => onChange(newValue.map((value) => value.name))}
                         components={animatedComponents}
@@ -283,15 +333,16 @@ export const RecipeEditForm = () => {
               <Controller
                 control={control}
                 name="ingredients"
+                rules={{ required: 'Это поле обязательное' }}
                 render={({ field: { onChange }, fieldState: { error } }) => (
                   <div>
                     <div className={styles.select__group}>
                       <Select
                         className={styles.select}
                         options={ingredients}
+                        defaultValue={editingRecipe.ingredients}
                         getOptionLabel={(ingredient: Ingredient) => ingredient.name}
                         getOptionValue={(ingredient: Ingredient) => ingredient.name}
-                        defaultValue={editingRecipe.ingredients}
                         closeMenuOnSelect={false}
                         onChange={(newValue) => onChange(newValue.map((value) => value.name))}
                         components={animatedComponents}
@@ -321,13 +372,13 @@ export const RecipeEditForm = () => {
         </div>
         <div className={styles.btn__container}>
           <button className={`${styles.btn} ${styles.success}`} type="submit">
-            Сохранить
+            Создать
           </button>
           <button
             className={`${styles.btn} ${styles.cancel}`}
             onClick={() => {
-              setIsEdit(false);
               setIsOpen('recipeModalIsOpen', !recipeModalIsOpen);
+              setIsEdit(false);
             }}
             type="button">
             Отменить
